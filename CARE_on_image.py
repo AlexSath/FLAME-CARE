94e773e80602660f6a3a3ba791894eebce17a7fc
AlexSath <sathler10@gmail.com>
2025-07-05 19:01:43 -0700

    started MATLAB_README.md

---
+++ CARE_on_image.py
@@ -0,0 +1,123 @@
+import os, json, logging, argparse, subprocess
+from sys import argv
+from pathlib import Path # Python 3.5+
+from datetime import datetime
+
+import mlflow
+from mlflow.client import MlflowClient
+from matlab import engine as matlab_engine
+
+from flame.image import FLAMEImage
+from flame.engine import CAREInferenceSession
+from flame.error import FLAMEMLFlowError, FLAMECmdError, FLAMEIOError, FLAMEPyMatlabError
+from flame.io import assert_path_exists, assert_direc_exists, assert_file_exists
+from flame.utils import set_up_tracking_server, update_matlab_variables
+
+INIT_TIME = datetime.now().strftime('%Y%m%d-%H%M%S')
+LOG_DIREC = os.path.join(Path.home(), "logs")
+if not os.path.isdir(LOG_DIREC): os.mkdir(LOG_DIREC)
+LOGGER = logging.getLogger("MAIN")
+logging.basicConfig(
+    filename=os.path.join(LOG_DIREC, f"{INIT_TIME}_CARE-on-image.log"),
+    encoding="utf-8",
+    level=logging.DEBUG
+)
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="CARE_on_data.py",
+        description= "Use CARE (content-aware image restoration) to denoise data.\n" \
+            + " - Data can be a single tiff or a folder of tiffs.\n" \
+            + " - Uses MLFlow (v. 2-22-2) Registered Models to track and retrieve models.\n"
+    )
+
+    meta_group = parser.add_argument_group("Meta Parameters")
+    meta_group.add_argument("--matlab", action="store_true", help="Whether to coordinate with a running MATLAB/FLAME `sessionPostProcessing` thread.")
+    meta_group.add_argument("--data-path", required=("--matlab" not in argv), help="The path to the data to infer on if '--matlab' is not requested.")
+    
+    model_group = parser.add_argument_group("Model Information", description="Variables to configure the name and version of the requested CARE model.")
+    model_group.add_argument("--model-name", required=True, type=str, choices=["CARE-1Channel"], help="The name of the 'Registered Model' to pull from")
+    model_group.add_argument("--model-version", type=int, required=False, help="The version of the registered model to pull from. If none is provided, most recent is used.")
+
+    mlflow_group = parser.add_argument_group("MLFlow Tracking", description="Variables to configure MLFlow tracking for model retrieval.")
+    mlflow_group.add_argument("--mlflow-tracking-direc", required=True, help="Directory with 'mlruns' folder. Try '<mount>/SynologyDrive/CARE_for_MATLAB/mlruns'.")
+    mlflow_group.add_argument("--mlflow-tracking-ip", required=False, default="127.0.0.1", help="IP address where to host the MLFlow server.")
+    mlflow_group.add_argument("--mlflow-tracking-port", required=False, default="5050", help="Port where to run the MLFlow server")
+
+    args = parser.parse_args()
+
+    """PARAMETER INPUT VALIDATION"""
+    if args.data_path is not None:
+        DATA_PATH_TYPE = None
+        assert_path_exists(args.data_path)
+        try: 
+            assert_file_exists(args.data_path)
+            DATA_PATH_TYPE = "file"
+        except FLAMEIOError as e: DATA_PATH_TYPE = "directory"
+    assert_direc_exists(args.mlflow_tracking_direc)
+    
+    """SET UP MLFLOW SERVER PROCESS"""
+    MLFLOW_SERVER_PROCESS = set_up_tracking_server(
+        ip=args.mlflow_tracking_ip,
+        port=args.mlflow_tracking_port,
+        direc=args.mlflow_tracking_direc,
+        log_path=os.path.join(LOG_DIREC, f"{INIT_TIME}_mlflow_server.log")
+    )
+
+    """DOWNLOAD MLFLOW ARTIFACTS FOR INFERENCE"""
+    try:
+        TRACKING_URI = f"http://{args.mlflow_tracking_ip}:{args.mlflow_tracking_port}"
+        CLIENT = MlflowClient(tracking_uri=TRACKING_URI)
+        REGISTERED_MODEL = CLIENT.get_registered_model(args.model_name)
+
+        if args.model_version is None:
+            THIS_RUN_ID = REGISTERED_MODEL.latest_versions[-1].run_id
+        else:
+            THIS_RUN_ID = REGISTERED_MODEL.latest_versions[args.model_version - 1].run_id
+    except Exception as e:
+        LOGGER.error(
+            f"Could not get {args.model_name} v.{args.model_version} from registered models at provided tracking server.\n" \
+          + f"{e.__class__.__name__}: {e}"
+        )
+        raise FLAMEMLFlowError(
+            f"Could not get {args.model_name} v.{args.model_version} from registered models at provided tracking server.\n" \
+          + f"{e.__class__.__name__}: {e}"
+        )
+        
+    """INITIALIZING MODEL INFERENCE ENGINE"""
+    try:
+        ENGINE = CAREInferenceSession.from_mlflow_uri(
+            tracking_uri=TRACKING_URI,
+            run_id=THIS_RUN_ID
+        )
+    except Exception as e:
+        LOGGER.error(f"Could not initialize CAREInferenceSession from {THIS_RUN_ID}.{e.__class__.__name__}: {e}")
+        raise FLAMECmdError(f"Could not initialize CAREInferenceSession from {THIS_RUN_ID}.{e.__class__.__name__}: {e}")
+    
+    """DETERMINING IF IN MATLAB MODE"""
+    if args.matlab == True:
+        try:
+            matlab_engine.connect_matlab()
+        except Exception as e:
+            LOGGER.exception(f"Could not connect to matlab engine.\n{e.__class__.__name__}: {e}")
+            raise FLAMEPyMatlabError(f"Could not connect to matlab engine.\n{e.__class__.__name__}: {e}")
+        
+        matlab_engine_variable_names = [
+            "PYTHON_INFERENCE_ACTIVE", "CURRENT_IMAGE"
+        ]
+        try:
+            update_matlab_variables(
+                {x: None for x in matlab_engine_variable_names}
+            )
+        except Exception as e:
+            LOGGER.exception(f"Could not sync variables to matlab engine.\n{e.__class__.__name__}: {e}")
+            raise FLAMEPyMatlabError(f"Could not sync variables to matlab engine.\n{e.__class__.__name__}: {e}")
+        
+
+    
+    
+
+    subprocess.Popen.kill(MLFLOW_SERVER_PROCESS)
+
+if __name__ == "__main__":
+    main()
---
+++ MATLAB_README.md
@@ -0,0 +1,6 @@
+### Use this readme if running inference through MATLAB.
+
+The following instructions should be used if you would like to run CARE inference for FLAME images through
+MATLAB.
+
+There are two possible implementations. Both require the `CARE_on_image.py` script.
